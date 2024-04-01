3e4f6ae4d8c9f56f7f1b0d349b3dcc5d9411925d
Acidnyi <sergeykuklich@gmail.com>
2024-04-01 13:25:54 +0200

    [DATABASE] updated database template

--- database_template.sql
+++ database_template.sql
@@ -1,36 +1,36 @@
 CREATE TABLE "images"(
-    "id" BIGINT NOT NULL,
+    "id" SERIAL PRIMARY KEY NOT NULL,
     "url" BIGINT NOT NULL,
     "event_id" BIGINT NOT NULL
 );
-ALTER TABLE
-    "images" ADD PRIMARY KEY("id");
+
+
 CREATE TABLE "chats"(
-    "id" BIGINT NOT NULL,
+    "id" SERIAL PRIMARY KEY NOT NULL,
     "owner_id" BIGINT NOT NULL,
     "event_id" BIGINT NOT NULL,
     "message" TEXT NOT NULL
 );
-ALTER TABLE
-    "chats" ADD PRIMARY KEY("id");
+
 CREATE TABLE "users"(
-    "id" BIGINT NOT NULL,
+    "id" SERIAL PRIMARY KEY NOT NULL,
     "name" VARCHAR(32) NOT NULL,
     "surname" VARCHAR(50) NOT NULL,
     "email" VARCHAR(255) NOT NULL,
-    "password" TEXT NOT NULL
+    "password" TEXT NOT NULL,
+	"birth_date" DATE NOT NULL
 );
-ALTER TABLE
-    "users" ADD PRIMARY KEY("id");
+
+
 CREATE TABLE "event_inventory"(
-    "id" BIGINT NOT NULL,
+    "id" SERIAL PRIMARY KEY NOT NULL,
     "user_id" BIGINT NOT NULL,
+    "birth_date" DATE NOT NULL,
     "event_id" BIGINT NOT NULL
 );
-ALTER TABLE
-    "event_inventory" ADD PRIMARY KEY("id");
+
 CREATE TABLE "events"(
-    "id" BIGINT NOT NULL,
+    "id" SERIAL PRIMARY KEY NOT NULL,
     "name" VARCHAR(32) NOT NULL,
     "interpret" VARCHAR(32) NOT NULL,
     "place" VARCHAR(32) NOT NULL,
@@ -40,22 +40,19 @@ CREATE TABLE "events"(
     "detail" TEXT NOT NULL,
     "iamges" BIGINT NOT NULL
 );
-ALTER TABLE
-    "events" ADD PRIMARY KEY("id");
+
 CREATE TABLE "messages"(
-    "id" BIGINT NOT NULL,
+    "id" SERIAL PRIMARY KEY NOT NULL,
     "owner_id" BIGINT NOT NULL
 );
-ALTER TABLE
-    "messages" ADD PRIMARY KEY("id");
+
 CREATE TABLE "tickets"(
-    "id" BIGINT NOT NULL,
+    "id" SERIAL PRIMARY KEY NOT NULL,
     "owner_id" BIGINT NOT NULL,
     "event_id" BIGINT NOT NULL,
     "qr_code" bytea NOT NULL
 );
-ALTER TABLE
-    "tickets" ADD PRIMARY KEY("id");
+
 ALTER TABLE
     "images" ADD CONSTRAINT "images_event_id_foreign" FOREIGN KEY("event_id") REFERENCES "events"("id");
 ALTER TABLE
